ddcd73afdc1807a2281a4ae9db1b5954dced7b9d
Eugene Drapoguz <eugene.drapoguz@gmail.com>
2019-03-27 14:26:10 +0200

    Add Editor UI Module

---
+++ app/src/main/java/com/eugene/boost/ui/project/editor/EditorFragment.kt
@@ -0,0 +1,34 @@
+package com.eugene.boost.ui.project.editor
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.eugene.boost.R
+import com.eugene.boost.ui.base.BaseFragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class EditorFragment : BaseFragment() {
+
+    private val editorViewModel: EditorViewModel by viewModel()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_project_editor, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+    }
+
+
+    private fun initView() {
+
+    }
+}
---
+++ app/src/main/java/com/eugene/boost/ui/project/editor/EditorViewModel.kt
@@ -0,0 +1,5 @@
+package com.eugene.boost.ui.project.editor
+
+import com.eugene.boost.ui.base.BaseViewModel
+
+class EditorViewModel : BaseViewModel()
