48022a6e0471b23e94debae26fd051da22f6e6aa
Eugene Drapoguz <eugene.drapoguz@gmail.com>
2019-04-01 15:11:30 +0300

    Move Empty State to a Separate View

--- app/src/main/java/com/eugene/boost/ui/project/projects/ProjectsFragment.kt
+++ app/src/main/java/com/eugene/boost/ui/project/projects/ProjectsFragment.kt
@@ -8,7 +8,10 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.eugene.boost.R
 import com.eugene.boost.ui.base.BaseFragment
+import com.eugene.boost.util.gone
+import com.eugene.boost.util.visible
 import kotlinx.android.synthetic.main.fragment_project_projects.*
+import kotlinx.android.synthetic.main.view_empty_screen.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ProjectsFragment : BaseFragment() {
@@ -35,8 +38,21 @@ class ProjectsFragment : BaseFragment() {
 
         setToolbarTitle(getString(R.string.app_projects))
 
+        img_empty_screen_icon.setImageResource(R.drawable.ic_folder_open_black_24dp)
+        txt_empty_screen_description.setText(R.string.project_projects_empty_screen_description)
+        viw_empty_screen.visible()
+
         _projectsViewModel.isLoading.observe(this, Observer {
 
+        })
+
+        _projectsViewModel.projects.observe(this, Observer {
+
+            if (it.isNotEmpty()) {
+                viw_empty_screen.gone()
+            } else {
+                viw_empty_screen.visible()
+            }
         })
 
         _projectsViewModel.loadProjects()
